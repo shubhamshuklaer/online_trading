bd656403a4fa233fae8dbd916a169e5b783c4081
shubhamshuklaer <shubhamshuklaerssss@gmail.com>
2014-04-08 00:01:44 +0530

    Added basic search functionality

--- files/search_process.php
+++ files/search_process.php
@@ -7,8 +7,20 @@ if(isset($_GET["search_bar"])){
 	else
 		$correct_spell=0;
 	$omysql=new MySQL();
-	$where=array("item_nm like"=>$search_text."%");
-	$omysql->Select("items",$where);
-	
+	$where=array("search_term ="=>$search_text);
+	$cols="item_id,rep_name as rank";
+	$order_by="rank DESC";
+	$omysql->Select("search_index",$where,$order_by,"",$cols);
+	$search_items=$omysql->arrayedResult;
+	$item_ids=array();
+	foreach ($search_items as $row) {
+		$item_ids[]=$row["item_id"];
+	}
+	$query="Select * from items where item_id IN (".implode(",",$item_ids).") order by FIELD( item_id,".implode(",",$item_ids).") ";
+	/* 
+	as item_ids are taken form database itself and 
+	they are ints so no need for sanitization
+	*/
+	$omysql->ExecuteSQL($query);
 }
 ?>
--- files/show_result.php
+++ files/show_result.php
@@ -3,7 +3,7 @@ if($omysql->records>0){
 	$result=$omysql->arrayedResult;
 	foreach($result as $row){
 		echo "<li>";
-		echo $row["item_nm"];
+		echo $row["item_id"];
 		echo "</li>";
 	}
 }

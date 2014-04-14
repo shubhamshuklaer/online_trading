5acdf5fe2a4a63c86f6cce4b28b75f6d485bc8db
shubhamshuklaer <shubhamshuklaerssss@gmail.com>
2014-04-14 10:29:39 +0530

    Before merging profile made a backup of database

--- online_trading_before_mearging_profile.sql
+++ online_trading_before_mearging_profile.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 07, 2014 at 01:20 PM
+-- Generation Time: Apr 14, 2014 at 04:57 AM
 -- Server version: 5.5.35-0ubuntu0.13.10.2
--- PHP Version: 5.5.3-1ubuntu2.2
+-- PHP Version: 5.5.3-1ubuntu2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -57,6 +57,18 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `categories_name` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`categories_name`) VALUES
+('Electronics'),
+('Electronics:Fan'),
+('Mobile'),
+('Laptop'),
+('testing'),
+('testing:test');
+
 -- --------------------------------------------------------
 
 --
@@ -78,17 +90,19 @@ CREATE TABLE IF NOT EXISTS `items` (
   `category` varchar(100) NOT NULL,
   `last_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `items`
 --
 
 INSERT INTO `items` (`item_nm`, `pic_loc`, `user_nm`, `item_id`, `quantity`, `cost`, `item_condition`, `description`, `timestamp`, `type`, `promotion_amnt`, `category`, `last_date`) VALUES
-('how are u', 'dsfs', 'sdjfhas', 1, 1, 1, 'sdgad', 'gsaddgsad', '2014-04-02 11:10:02', 'sgdas', 111, 'sadfsa', '0000-00-00 00:00:00'),
-('how are u', 'dsfs', 'sdjfhas', 2, 1, 1, 'sdgad', 'gsaddgsad', '2014-04-02 11:10:11', 'sgdas', 111, 'sadfsa', '0000-00-00 00:00:00'),
-('how are u', 'dsfs', 'sdjfhas', 3, 1, 1, 'sdgad', 'gsaddgsad', '2014-04-02 11:10:17', 'sgdas', 111, 'sadfsa', '0000-00-00 00:00:00'),
-('how are u', 'dsfs', 'sdjfhas', 4, 1, 1, 'sdgad', 'gsaddgsad', '2014-04-02 11:10:22', 'sgdas', 111, 'sadfsa', '0000-00-00 00:00:00');
+('how are u', 'abc.jpg', 'sdjfhas', 1, 1, 100, 'sdgad', 'gsaddgsad', '2014-04-12 19:11:13', 'auction', 1, 'Electronics', '2014-04-14 00:50:00'),
+('how are u', 'abc.jpg', 'sdjfhas', 2, 1, 150, 'sdgad', 'gsaddgsad', '2014-04-14 04:14:59', 'auction', 1, 'Electronics', '0000-00-00 00:00:00'),
+('how are u', 'abc.jpg', 'sdjfhas', 3, 1, 50, 'sdgad', 'gsaddgsad', '2014-04-14 04:14:59', 'sale', 1, 'Electronics', '0000-00-00 00:00:00'),
+('how are u', 'bcd.jpg', 'sdjfhas', 4, 1, 70, 'sdgad', 'gsaddgsad', '2014-04-14 04:12:01', 'sale', 2, 'Electronics', '0000-00-00 00:00:00'),
+('gdskljg', 'abc.jpg', 'shubham', 5, 1, 700, 'dsfh', 'kljl', '2014-04-14 04:12:01', 'auction', 1, 'Electronics', '0000-00-00 00:00:00'),
+('Mobile', 'abc.jpg', 'shubham', 6, 1, 1050, 'new', 'fjasklfkls', '2014-04-14 04:14:59', 'sale', 11, 'Electronics:Mobile', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -102,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `item_id` int(11) NOT NULL,
   `txn_id` int(11) NOT NULL,
   `order_id` varchar(50) NOT NULL,
-  `order_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `order_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `delivery_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `status` varchar(25) NOT NULL,
   PRIMARY KEY (`order_id`)
@@ -135,6 +149,36 @@ INSERT INTO `search_history` (`user_nm`, `search_text`, `search_id`, `timestamp`
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `search_index`
+--
+
+CREATE TABLE IF NOT EXISTS `search_index` (
+  `search_term` varchar(20) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `rep_name` int(11) NOT NULL DEFAULT '0',
+  `rep_category` int(11) NOT NULL DEFAULT '0',
+  `rep_description` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`search_term`,`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `search_index`
+--
+
+INSERT INTO `search_index` (`search_term`, `item_id`, `rep_name`, `rep_category`, `rep_description`) VALUES
+('gdsa', 1, 0, 1, 1),
+('go', 1, 0, 0, 1),
+('hello', 1, 1, 0, 1),
+('hello', 2, 1, 0, 0),
+('hello', 3, 1, 0, 0),
+('hello', 4, 1, 0, 0),
+('hello', 5, 6, 0, 0),
+('hello', 6, 1, 0, 0),
+('rain', 1, 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -157,7 +201,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 CREATE TABLE IF NOT EXISTS `watch_list` (
   `user_nm` varchar(100) NOT NULL,
   `item_id` int(11) NOT NULL,
-  PRIMARY KEY (`item_id`)
+  PRIMARY KEY (`user_nm`,`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
